53f87e7cafd0c075396e59a10e8d7fdf4e0d7e01
Liam <kelley@bu.edu>
2025-04-02 13:44:13 -0400

    Added usage line in file

--- Scripts/XMLtoJSON.py
+++ Scripts/XMLtoJSON.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+# usage: ./XMLtoJSON.py <input XML file> <output JSON file>
+
 import sys
 import cobra
 
-model = cobra.io.read_sbml_model(str(sys.argv[1]))
-cobra.io.save_json_model(model, str(sys.argv[2]))
+model = cobra.io.read_sbml_model(sys.argv[1])
+cobra.io.save_json_model(model, sys.argv[2])
 
 print("Success")
