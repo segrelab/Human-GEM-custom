577010ad44fe300607faac70ee846ba459c9e7e5
Liam <kelley@bu.edu>
2025-04-02 16:01:54 -0400

    Preliminary R analysis code

---
+++ R/GEM_analysis.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(magrittr)
+
+reactions <- read_csv("Data/reactions.csv") %>%
+  select(-1) %>%
+  mutate(across(c(Subsystem, Genes, EC, KEGG, BiGG, VMH), as.factor))
+
+metabolites <- read_csv("Data/metabolites.csv") %>%
+  select(-1) %>%
+  mutate(across(c(Name, Formula, Compartment, KEGG, BiGG, ChEBI, VMH, Reactions), as.factor),
+         ID_without_prefix = substr(ID, 0, 8) %>% as.factor()) %>%
+  relocate(ID_without_prefix, .after = ID)
+
+genes <- read_csv("Data/genes.csv") %>%
+  select(-1) %>%
+  mutate(across(c(Uniprot, Reactions), as.factor))
+  
